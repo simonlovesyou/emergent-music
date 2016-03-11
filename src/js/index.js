6257f2c4fccd138ac4f9b9cbf8e9ac4e1858a986
Jakob Åberg <jakobwilliamaberg@gmail.com>
2016-03-11 18:18:48 +0100

    Added dropdown for instruments and range slider for setting the bpm of a song

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,7 @@ const soundFont = new Soundfont(context);
 let piano;
 let song = [];
 let numberOfMoves = 0;
+let instrument = 'gunshot';
 
 
 /*Class note. A note contains a note, frequency, the duration of the note, chordwill and placement.*/
@@ -307,23 +308,39 @@ xhr.onreadystatechange = function() {
       console.log(n.note.name() + " " + n.position);
     });*/
 
-
     console.log(song);
 
-    song.sort((n1, n2) => (n1.position < n2.position) ? -1 : 1);
+    //instrument = instrumental(); 
 
-    console.log(song.map(n => n.note.name() + n.position).join(' '));
-
-    //Load instrument as a soundfount
-    piano = soundFont.instrument('gunshot');
-    document.querySelector('#play').innerHTML = 'Loading...';
-    document.querySelector('#play').disabled = true;
-    piano.onready(() => {
-      document.querySelector('#play').innerHTML = 'Play';
-      document.querySelector('#play').disabled = false;
-    });
+    readyPiano(instrument);
 
   }
+}
+
+/*Function to choose instrument from the dropdown list*/
+function instrumental(){
+  let selectID = document.getElementById("instrumentDrop");
+  let instrument = selectID.options[selectID.selectedIndex].value;
+  readyPiano(instrument);
+}
+
+
+/*Function to set up the piano*/
+function readyPiano(instrument){
+
+  console.log(instrument);
+  song.sort((n1, n2) => (n1.position < n2.position) ? -1 : 1);
+
+  console.log(song.map(n => n.note.name() + n.position).join(' '));
+
+  //Load instrument as a soundfount
+  piano = soundFont.instrument(instrument);
+  document.querySelector('#play').innerHTML = 'Loading...';
+  document.querySelector('#play').disabled = true;
+  piano.onready(() => {
+    document.querySelector('#play').innerHTML = 'Play';
+    document.querySelector('#play').disabled = false;
+  });
 }
 
 function iteration() {
@@ -355,12 +372,15 @@ let playing = false;
 
 function play() {
 
+  let speed = document.querySelector('#speedInput').value;
   let start = 0;
+
+  console.log(speed);
   //Iterate through all notes
   song.forEach((note) => {
 
     //Duration for current note
-    let duration = noteDuration(note.duration, 650);  //note measure, bpm
+    let duration = noteDuration(note.duration, speed);  //note measure, bpm
     //Play the note after a certain amount of time
     //console.log(note.duration + note.note.name());
 
