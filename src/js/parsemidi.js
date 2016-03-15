be85c283c37b4202bcf4dc1a5aa30e5404728566
simonlovesyou <simonljohansson92@gmail.com>
2016-03-15 19:36:06 +0100

    add a second output file

--- src/js/parsemidi.js
+++ src/js/parsemidi.js
@@ -6,27 +6,15 @@ var noteOff = new Buffer('803c00', 'hex'); // Channel 0, middle C4, 0 velocity
 var message = midi.Message.fromBuffer(noteOff);
 let hexToMidi = {};
 
-/*fs.readFile(path.join(process.cwd(), '/src/midi.mid'), (err, data) => {
-  console.log(data);
-  console.log(midi.Track);
-  let message = midi.Track.fromBuffer(new Buffer('MTrk').join(data));
-
-  //console.log(message.getStatus()); // 0x80 
-  /*console.log(message.getCommand()); // "NOTE_OFF" 
-  console.log(message.getChannel()); // 0 
-  console.log(message.getData()); // [0x3c, 0x00]
-
-});*/
-
 
 fs.readFile(path.join(process.cwd(), '/src/table.txt'), 'utf8', (err, data) => {
   let temp = [];
   data.split('\n').forEach((d, index) => {
-    if(index < 132) {
+    if(index < 156) {
       temp.push([d]);
       hexToMidi[d] = undefined;
     } else {
-      temp[(index % 132)].push(d);
+      temp[(index % 156)].push(d);
     }
   });
   console.log(temp);
@@ -48,13 +36,14 @@ midi.Sequence.fromFile(path.join(process.cwd(), '/src/for_elise.mid'), (err, seq
   }
 
   console.log(sequence.getTracks());
+  console.log(hexToMidi);
 
   console.log(sequence.getTracks()[0].events);
 
   sequence.getTracks()[1].events.forEach((event) => {
     //console.log(event.message.getCommand());
     if(event.message.getCommand() === 'META_MESSAGE') {
-      console.log(event.message);
+      //console.log(event.message);
     }
 
 
@@ -62,7 +51,7 @@ midi.Sequence.fromFile(path.join(process.cwd(), '/src/for_elise.mid'), (err, seq
       console.log(event.message.getCommand().match(/NOTE_(ON|OFF)/)[0] + ": " + hexToMidi[event.message.getData()[0]] + " (" + event.message.getData()[0] + ")");
       //console.log("Duration: " + (Math.pow(2, 7) / event.delta));
 
-      console.log(event.message);
+      //console.log(event.message);
 
       if(event.message.getCommand().match(/(ON|OFF)/)[0] === 'ON') {
         notes.push(['ON', hexToMidi[event.message.getData()[0]], 1/(Math.pow(2, 8) / event.delta)]);
@@ -79,7 +68,7 @@ midi.Sequence.fromFile(path.join(process.cwd(), '/src/for_elise.mid'), (err, seq
   });
 
 
-  fs.writeFile(path.join(process.cwd(), '/src/assets/txt/supermario.txt'), notes.join('\n'), (err) => {
+  fs.writeFile(path.join(process.cwd(), '/src/assets/txt/for_elise.txt'), notes.join('\n'), (err) => {
     if(err) console.log("Fel!: " +err);
     console.log("Saved!");
   });
